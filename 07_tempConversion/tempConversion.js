f5076ddf089210df6903f3137c2f9260b23b7b20
klsegade <199424623+klsegade@users.noreply.github.com>
2025-04-05 00:54:28 -0700

    Completed exercise 7
    Not too shabby.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  cel = (fahr - 32)*(5/9)
+  return Math.round(cel * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  fahr = (cel*(9/5)) + 32
+  return Math.round(fahr * 10) / 10
 };
 
 // Do not edit below this line
