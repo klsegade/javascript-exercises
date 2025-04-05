d460a5fe35d5e6d0ab4a97b16a62438b16b5dbb0
klsegade <199424623+klsegade@users.noreply.github.com>
2025-04-04 21:34:47 -0700

    Completed exercise 5
    Figured it out. What else can I say.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (Number.isInteger(int1) && Number.isInteger(int2) && int1 > 0 && int2 >0) {        
+    }
+    else {
+        return 'ERROR'
+    }
+    sumArray = [int1,int2]
+    newArray = sumArray.sort()
+    sum = 0
+    for (let i = newArray[0]; i <= newArray[1]; i++) {
+        sum += i
+    }
+    return sum
 
 };
 
